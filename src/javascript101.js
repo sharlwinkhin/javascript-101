00899cd38be39b9ee4fe3601ab8f69655e75dbc4
sharlwinkhin <lkshar@smu.edu.sg>
2025-08-08 14:17:36 +0800

    created test cases for js 101 assignment

---
+++ src/javascript101.js
@@ -0,0 +1,33 @@
+
+/*
+    Complete computeAverageScore function so that it
+        - calculate average score of students
+        - return average score
+
+    Complete checkStatus function so that it
+        - check the score of a given student
+        - return "Pass" if the score is >= 50; otherwise "Fail"
+*/
+const students = [
+            { name: "Alice", score: 85 },
+            { name: "Bob", score: 67 },
+            { name: "Charlie", score: 42 }
+        ]
+
+function computeAverageScore() {
+    let result = 0
+    // add your code here
+
+    return result
+}
+
+function checkStatus(student) {
+    // add your code here
+
+    
+}
+
+computeAverageScore()
+checkStatus(students[1])
+checkStatus(students[2])
+
---
+++ test/javascript101.test.js
@@ -0,0 +1,17 @@
+const computeAverageScore = require('../src/javascript101')
+
+const students = [
+            { name: "Alice", score: 85 },
+            { name: "Bob", score: 67 },
+            { name: "Charlie", score: 43 }
+        ]
+
+// this test checks if computeAverageScore() correctly compute the average scores of a given students list
+test('computeAverageScore', () => {
+    expect(computeAverageScore(students).toBe(65))
+})
+
+// this test checks if checkStatus() correctly return the Pass/Fail status of a given student
+test('checkStatus', () => {
+    expect(checkStatus(students[2]).toBe('Fail'))
+})
